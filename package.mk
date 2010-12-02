737c56b29d8d050dd94b4a3ae68d9b4a5d4eff73
David Wragg <david@rabbitmq.com>
2010-12-02 12:32:34 +0000

    Overhaul for bug23568

--- package.mk
+++ package.mk
@@ -1,4 +1,1 @@
 DEPS:=rabbitmq-server rabbitmq-mochiweb mochiweb-wrapper erlang-rfc4627-wrapper
-RUNTIME_DEPS:=rabbitmq-mochiweb rfc4627_jsonrpc
-TEST_APPS:=rfc4627_jsonrpc crypto inets mochiweb rabbit_mochiweb rabbit_jsonrpc
-START_RABBIT_IN_TESTS:=true
